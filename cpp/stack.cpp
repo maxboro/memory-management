a0f049d16cb29f0a54560525fb3714e0ea70519b
Max B. <maksym_borovyk@ukr.net>
2025-01-24 21:38:30 +0200

    Add stack example for cpp

---
+++ cpp/stack.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    // Declare an integer on the stack and initialize it to 5.
+    int stack_value = 5;
+
+    // Print the memory address of the stack variable.
+    std::cout << &stack_value << std::endl;
+
+    // Print the value stored in the stack variable.
+    std::cout << stack_value << std::endl;
+
+    // There is no need to manually free stack memory.
+    // The memory for `stack_value` will automatically be reclaimed when the variable goes out of scope.
+    return 0;
+}
