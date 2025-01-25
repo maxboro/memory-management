e275a0e413afea60abe0a67610de8132a63ebfd6
Max B. <maksym_borovyk@ukr.net>
2025-01-25 12:22:33 +0200

    Add more info to prints in C stack example

--- c/stack.c
+++ c/stack.c
@@ -6,10 +6,10 @@ int main()
     int stack_value = 5;
 
     // Print the memory address of the stack variable.
-    printf("%p\n", &stack_value);
+    printf("Stack value memory adress: %p\n", &stack_value);
 
     // Print the value stored in the stack variable.
-    printf("%d", stack_value);
+    printf("Stack value: %d", stack_value);
 
     // There is no need to manually free stack memory.
     // The memory for `stack_value` will automatically be reclaimed when the variable goes out of scope.
