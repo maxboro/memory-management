535599ebd09c00ebef57132dd848856e7e0c587f
Max B. <maksym_borovyk@ukr.net>
2025-01-25 12:57:08 +0200

    Capitalize prints in heap.c

--- c/heap.c
+++ c/heap.c
@@ -5,10 +5,10 @@ int main()
 {
     // Variable declared in memory statically allocated on stack
     int value = 5;
-    printf("value stack: %d\n", value);
+    printf("Value stack: %d\n", value);
     
     // size of the variable `value`
-    printf("size: %ld bytes\n", sizeof(value));
+    printf("Size: %ld bytes\n", sizeof(value));
     
     // Dynamically allocate memory for one integer on heap
     int *value_ptr = malloc(sizeof(value));
@@ -16,17 +16,17 @@ int main()
         fprintf(stderr, "Memory allocation failed\n");
         return 1;
     }
-    printf("heap mem loc: %p\n", (void *)value_ptr);
+    printf("Heap mem loc: %p\n", (void *)value_ptr);
     
     // Assign a value to the dynamically allocated memory on heap
     *value_ptr = 7;
-    printf("value heap: %d\n", *value_ptr);
+    printf("Value heap: %d\n", *value_ptr);
     
     // Free the dynamically allocated memory on heap
     free(value_ptr);
     
     // Accessing freed memory is undefined behavior; the value is no longer valid
-    printf("value heap after free %d\n", *value_ptr);
+    printf("Value heap after free %d\n", *value_ptr);
     
     return 0;
 }
